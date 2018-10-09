ba4da49abc5b96981aed84392e121ef2efdb4019
Unknown <nol_computernerd@hotmail.com>
2018-10-09 20:24:29 +0200

    Added pagination and Itemsshownperpage

--- TCGshopTestEnvironment/Controllers/ProductsController.cs
+++ TCGshopTestEnvironment/Controllers/ProductsController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using TCGshopTestEnvironment.Services;
 using TCGshopTestEnvironment.ViewModels;
-
+using X.PagedList.Mvc;
+using X.PagedList;
 namespace TCGshopTestEnvironment.Controllers
 {
     public class ProductsController : Controller
@@ -17,10 +19,13 @@ namespace TCGshopTestEnvironment.Controllers
             _assets = assets;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? page, int? pageAmount)
         {
+            ViewBag.page = page;
+            ViewBag.PageAmount = pageAmount;
             var assetModels = _assets.GetAll();
-
+            var pageNumber = page ?? 1;
+            var pageAmnt = pageAmount ?? 10;
             var listingResult = assetModels
                 .Select(result => new ProductsViewModel
                 {
@@ -30,16 +35,16 @@ namespace TCGshopTestEnvironment.Controllers
                     ImageUrl = result.ImageUrl,
                     Grade = result.Grade,
                     Stock = result.Stock
-                    
-                    
 
                 });
             var model = new ProductsIndexModel()
             {
                 Assets = listingResult
-            }
-            ;
-            return View(model);
+            };
+            var onePageOfProducts = listingResult.ToPagedList(pageNumber, pageAmnt);
+            ViewBag.OnePageOfProducts = onePageOfProducts;
+
+            return View();
         }
 
         public IActionResult Detail(int id)
