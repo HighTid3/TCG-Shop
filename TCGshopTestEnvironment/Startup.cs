12e5383d830419ce8020bcf97d97544c07400979
Jurre <jurre@koetse.eu>
2018-10-11 08:54:40 +0200

    Heroku Database

--- TCGshopTestEnvironment/Startup.cs
+++ TCGshopTestEnvironment/Startup.cs
@@ -74,21 +74,30 @@ namespace TCGshopTestEnvironment
             // Heroku provides PostgreSQL connection URL via env variable
             var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
 
-            // Parse connection URL to connection string for Npgsql
-            connUrl = connUrl.Replace("postgres://", string.Empty);
+            var connStr = "";
 
-            var pgUserPass = connUrl.Split("@")[0];
-            var pgHostPortDb = connUrl.Split("@")[1];
-            var pgHostPort = pgHostPortDb.Split("/")[0];
+            if (connUrl == null)
+            {
+                connStr = connection;
+            }
+            else
+            {
 
-            var pgDb = pgHostPortDb.Split("/")[1];
-            var pgUser = pgUserPass.Split(":")[0];
-            var pgPass = pgUserPass.Split(":")[1];
-            var pgHost = pgHostPort.Split(":")[0];
-            var pgPort = pgHostPort.Split(":")[1];
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
 
-            connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
-            
+                var pgUserPass = connUrl.Split("@")[0];
+                var pgHostPortDb = connUrl.Split("@")[1];
+                var pgHostPort = pgHostPortDb.Split("/")[0];
+
+                var pgDb = pgHostPortDb.Split("/")[1];
+                var pgUser = pgUserPass.Split(":")[0];
+                var pgPass = pgUserPass.Split(":")[1];
+                var pgHost = pgHostPort.Split(":")[0];
+                var pgPort = pgHostPort.Split(":")[1];
+
+                connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+            }
             
             services.AddDbContext<DBModel>(options => options.UseNpgsql(connStr));
 
