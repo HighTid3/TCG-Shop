2515c8e52925b1a65da0ebfd3df3a9d45a1a2a66
Jurre1996 <KoetseJ@users.noreply.github.com>
2018-10-11 17:42:16 +0200

    Update Startup.cs

--- TCGshopTestEnvironment/Startup.cs
+++ TCGshopTestEnvironment/Startup.cs
@@ -69,7 +69,28 @@ namespace TCGshopTestEnvironment
             services.AddSingleton(Configuration);
             services.AddScoped<IProducts, ProductService>();
             var connection = @"User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=TCG;Pooling=true;";
-            services.AddDbContext<DBModel>(options => options.UseNpgsql(connection));
+            
+            
+            // Heroku provides PostgreSQL connection URL via env variable
+            var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            // Parse connection URL to connection string for Npgsql
+            connUrl = connUrl.Replace("postgres://", string.Empty);
+
+            var pgUserPass = connUrl.Split("@")[0];
+            var pgHostPortDb = connUrl.Split("@")[1];
+            var pgHostPort = pgHostPortDb.Split("/")[0];
+
+            var pgDb = pgHostPortDb.Split("/")[1];
+            var pgUser = pgUserPass.Split(":")[0];
+            var pgPass = pgUserPass.Split(":")[1];
+            var pgHost = pgHostPort.Split(":")[0];
+            var pgPort = pgHostPort.Split(":")[1];
+
+            connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+            
+            
+            services.AddDbContext<DBModel>(options => options.UseNpgsql(connStr));
 
             services.AddSingleton<IEmailSender, EmailSender>();
             services.Configure<AuthMessageSenderOptions>(Configuration);
