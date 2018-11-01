320efb222b2d705bd3abc868130565e7eae05186
Jurre1996 <jurre@koetse.eu>
2018-11-02 00:17:45 +0100

    UI Changes

--- TCGshopTestEnvironment/wwwroot/js/NewProduct.js
+++ TCGshopTestEnvironment/wwwroot/js/NewProduct.js
@@ -38,3 +38,44 @@ $('#NewProduct').submit(function () {
         $("#HiddenCategroyInput").append('<input type="hidden" name="Category[]" value="' + Chips[i].tag + '">');
     }
 });
+
+
+
+//This Might be a bad idea, #YOLO
+$('#NewProductChip').on("keyup", function () {
+    //Deleting Everything inside DIV
+    $("#HiddenCategroyInput").empty();
+
+    //Deleting Everything inside DIV
+    $("#NewProductCategory").empty();
+
+    //Get all variables from the chips
+    var Chips = $('.catagory-chips').material_chip('data');
+
+    //loop trough all chips, and add them to an hidden div.
+    for (i = 0; i < Chips.length; ++i) {
+        console.log(Chips[i].tag);
+        //For sending Form
+        $("#HiddenCategroyInput").append('<input type="hidden" name="Category[]" value="' + Chips[i].tag + '">');
+
+        //For Filling Preview
+        $("#NewProductCategory").append('<div class="chip">' + Chips[i].tag + '</div> ');
+
+    }
+
+
+});
+
+
+
+$('#Name').bind('input',function() {
+    $('#NewProductName').text($('#Name').val());
+});
+
+$('#Grade').bind('input', function () {
+    $('#NewProductGrade').text($('#Grade').val());
+});
+
+$('#Price').bind('input', function () {
+    $('#NewProductPrice').text("€" + $('#Price').val());
+});
