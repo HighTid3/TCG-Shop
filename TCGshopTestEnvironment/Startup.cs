f8644a23a410dbb97f744cb58651884386e747a1
René <nol_computernerd@hotmail.com>
2018-11-12 12:46:42 +0100

    fixed js

--- TCGshopTestEnvironment/Startup.cs
+++ TCGshopTestEnvironment/Startup.cs
@@ -79,7 +79,7 @@ namespace TCGshopTestEnvironment
             services.AddScoped<IProducts, ProductService>();
             services.AddScoped<IShopping, ShoppingService>();
 
-            var connection = @"User ID=postgres;Password=postgres;Host=localhost;Port=5432;Database=TCG;Pooling=true;";
+            var connection = @"User ID=postgres;Password=test;Host=localhost;Port=5432;Database=TCG;Pooling=true;";
 
             
             // Heroku provides PostgreSQL connection URL via env variable
