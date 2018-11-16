62153a2964259a0a29937be0a8fd73fa4417c2c0
Furkan Ucar <furkanucar010@gmail.com>
2018-11-16 14:02:06 +0100

    EDIT DB

--- TCGshopTestEnvironment/Startup.cs
+++ TCGshopTestEnvironment/Startup.cs
@@ -80,7 +80,7 @@ namespace TCGshopTestEnvironment
             services.AddScoped<IShopping, ShoppingService>();
             services.AddScoped<IWishlist, WishlistService>();
 
-            var connection = @"User ID=postgres;Password=test;Host=localhost;Port=5432;Database=TCG;Pooling=true;";
+            var connection = @"User ID=postgres;Password=;Host=localhost;Port=5432;Database=TCG;Pooling=true;";
 
             
             // Heroku provides PostgreSQL connection URL via env variable
