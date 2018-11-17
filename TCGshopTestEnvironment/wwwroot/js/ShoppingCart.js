8a901f6c51f4f38246462d3d597b2f2640ac3f0a
Unknown <nol_computernerd@hotmail.com>
2018-11-17 14:24:30 +0100

    Auto stash before merge of "master" and "ReneTestEnvironment"

--- TCGshopTestEnvironment/wwwroot/js/ShoppingCart.js
+++ TCGshopTestEnvironment/wwwroot/js/ShoppingCart.js
@@ -1,50 +1,56 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
     shoppingCart.forEach(function (e) {
         var image = "<div class=\"shoppingCartImg\"><img src=\"" + storagePath + e.ImageUrl + ".png\" class=\"imageCell\" /></div>";
 
-/*        
-$('#my-ajax-table > tbody:last-child').append('<tr>');
-$('#my-ajax-table > tbody:last-child').append('<tr class="assetRow"><th>' + e.Id + '</th><th>' + e.Name + '</th><th>' + image + '</th><th>' + e.Price + '</th><th>' + e.Grade + '</th><th>' + e.Count + '</th></tr>');
-<div class="col-md-4"><p style="font-size:2.5rem;">' + e.Name + '</p></div><div class="col-md-4"><p> ' + e.Price + ' </p><p>' + e.Count + '</p></div></div></div>
-$('#my-ajax-table > tbody:last-child').append('<th style="100%;"><button class="btn btn-default btnSearch">Add to Wishlist</button></th></tr>'); 
+        /*        
+        $('#my-ajax-table > tbody:last-child').append('<tr>');
+        $('#my-ajax-table > tbody:last-child').append('<tr class="assetRow"><th>' + e.Id + '</th><th>' + e.Name + '</th><th>' + image + '</th><th>' + e.Price + '</th><th>' + e.Grade + '</th><th>' + e.Count + '</th></tr>');
+        <div class="col-md-4"><p style="font-size:2.5rem;">' + e.Name + '</p></div><div class="col-md-4"><p> ' + e.Price + ' </p><p>' + e.Count + '</p></div></div></div>
+        $('#my-ajax-table > tbody:last-child').append('<th style="100%;"><button class="btn btn-default btnSearch">Add to Wishlist</button></th></tr>'); 
+        
+        
+        $('#my-ajax-table > tbody:last-child').append('<tr>');
+                $('#my-ajax-table > tbody:last-child').append('<th class="ShoppingImage">' + image + '</th><th class="name">' + e.Name + '</th>');
+                $('th.name:last-child').append('<th class="ShoppingPrice"><br>Amount: <input class="ShoppingQuant text-center" type="number" placeholder="'+ e.Amount +'" value="'+ e.Amount +'"></th>');
+                $('#my-ajax-table > tbody:last-child').append('<th class=""><br>Price: € ' + e.Price + '</th>');
+        
+        */
 
 
-$('#my-ajax-table > tbody:last-child').append('<tr>');
-        $('#my-ajax-table > tbody:last-child').append('<th class="ShoppingImage">' + image + '</th><th class="name">' + e.Name + '</th>');
-        $('th.name:last-child').append('<th class="ShoppingPrice"><br>Amount: <input class="ShoppingQuant text-center" type="number" placeholder="'+ e.Amount +'" value="'+ e.Amount +'"></th>');
-        $('#my-ajax-table > tbody:last-child').append('<th class=""><br>Price: € ' + e.Price + '</th>');
+        $('#my-ajax-table > tbody:last-child').append('<div class="container"><div class="row lineCart"><div class="col-md-2 imageShoppingJs"> ' + image + '</div><div class="col-md-4 CartTitleColomn" style=""padding-left:210px;padding-right:10px;><h1 class="ShoppingCartTitle">' + e.Name + '</h1> Amount: <button class="btn btn-default btn-minPlus" id="' + e.ProductId + '">-</button> <input class="ShoppingQuant text-center" type="number" placeholder="' + e.Amount + '" value="' + e.Amount + '">  <button class="btn btn-default btn-minPlus" id="' + e.ProductId + '">+</button></div><div class="col-md-6 priceCart" style="font-size:2rem;" id="' + e.ProductId + '">Price: € ' + e.Price + ' p/u<div id = "RemoveItem"><br><a href="#" style="font-size:1.5rem;">Remove</a></div></div></div><hr style="width:750px;float:left;">');
+    });
 
-*/
+    $(function () {
 
+        $(".btn-minPlus").on("click", function () {
 
-        $('#my-ajax-table > tbody:last-child').append('<div class="container"><div class="row lineCart"><div class="col-md-2 imageShoppingJs"> ' + image + '</div><div class="col-md-4 CartTitleColomn" style=""padding-left:210px;padding-right:10px;><h1 class="ShoppingCartTitle">' + e.Name + '</h1> Amount: <button class="btn btn-default btn-minPlus" id="' + e.ProductId+ '">-</button> <input class="ShoppingQuant text-center" type="number" placeholder="'+ e.Amount +'" value="'+ e.Amount +'">  <button class="btn btn-default btn-minPlus" >+</button></div><div class="col-md-6 priceCart" style="font-size:2rem;">Price: € ' + e.Price + ' p/u<br><a href="#" style="font-size:1.5rem;">Remove</a></div></div><hr style="width:750px;float:left;">');
-       });
+            var $button = $(this);
+            var oldValue = $button.parent().find("input").val();
+            var cardid = $button.attr('id');//get product id
 
-$(function() {
+            if ($button.text() == "+") {
+                var newVal = parseFloat(oldValue) + 1;
+                addcardfromLocalstorage(cardid);
+                ShoppingcartBadge();
+            } else {
+                // Don't allow decrementing below zero
+                if (oldValue > 1) {
+                    var newVal = parseFloat(oldValue) - 1;
+                    removecardfromLocalstorage(cardid);
+                    ShoppingcartBadge();
+                } else {
+                    $button.parent().parent().parent().fadeOut(300);
+                    removecardfromLocalstorage(cardid);
+                    ShoppingcartBadge();
+                    newVal = 0;
+                }
+            }
 
-  $(".btn-minPlus").on("click", function() {
+            $button.parent().find("input").val(newVal);
 
-    var $button = $(this);
-    var oldValue = $button.parent().find("input").val();
+        });
 
-    if ($button.text() == "+") {
-      var newVal = parseFloat(oldValue) + 1;
-    } else {
-       // Don't allow decrementing below zero
-      if (oldValue > 1) {
-        var newVal = parseFloat(oldValue) - 1;
-      } else {
-          var id = $button.attr('id');
-          $button.parent().parent().fadeOut(300);
-          newVal = 0;
-      }
-      }
-
-    $button.parent().find("input").val(newVal);
-
-  });
-
-});
+    });
 
     $(function () {
         // Document.ready -> link up remove event handler
@@ -98,5 +104,19 @@ $(function() {
         //    }
         //}
     }
-});
 
+    function addcardfromLocalstorage(cardId) {
+        var cartindex = shoppingCart.findIndex((obj => obj.ProductId === cardId));
+        shoppingCart[cartindex].Amount += 1;
+        localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+    }
+
+    $('#RemoveItem a').click(function () {
+        var cardId = $(this).parent().parent().attr("id");
+        var cartindex = shoppingCart.findIndex((obj => obj.ProductId === cardId));
+        shoppingCart.splice(cartindex, 1);
+        localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
+        $(this).parent().parent().parent().parent().fadeOut(300);
+        ShoppingcartBadge();
+    });
+});
