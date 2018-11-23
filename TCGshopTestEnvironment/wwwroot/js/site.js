3357747bfba42aa351ad96c68ee4944c6249e7a8
René <nol_computernerd@hotmail.com>
2018-11-23 13:07:42 +0100

    Login fix

--- TCGshopTestEnvironment/wwwroot/js/site.js
+++ TCGshopTestEnvironment/wwwroot/js/site.js
@@ -244,9 +244,14 @@ $("#loginform").submit(function (e) {
         type: "POST",
         url: urls,
         data: form.serialize(), // serializes the form's elements.
-        success: function () {
-            AddLocalCartToDatabase(),  //perform the add local cart items to database cart
-                AddDbCarttoLocal();
+        success: function (response) {
+            if (response["status"] == "LoggedIn") {
+                AddLocalCartToDatabase(),  //perform the add local cart items to database cart
+                    AddDbCarttoLocal();
+            }
+            else {
+                $("#errormessages").html("Username or Password is incorrect.")
+            }
             
         }
 
