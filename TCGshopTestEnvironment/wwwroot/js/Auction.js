76719db8212d7a932f0299df022804801c7bec0c
Unknown <nol_computernerd@hotmail.com>
2018-12-19 00:09:32 +0100

    jurres autism is actin up again with datetime

--- TCGshopTestEnvironment/wwwroot/js/Auction.js
+++ TCGshopTestEnvironment/wwwroot/js/Auction.js
@@ -1,5 +1,6 @@
-﻿function CalculateAuctionTimer(beginTime, endTime) {
-    var countDownDate = endTime.getTime();
+﻿function CalculateAuctionTimer(id, beginTime, endTime) {
+
+    var countDownDate = new Date(moment.unix(endTime));
 
     // Update the count down every 1 second
     var x = setInterval(function () {
@@ -15,15 +16,29 @@
         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
+        var text = "<h4>";
         // Output the result in an element with id="demo"
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
+        if (!days < 1) {
+            text = text + days + "d ";
+        }
+        if (hours < 1 && days < 1 ) {
+            
+        }
+        else
+        {
+            text = text + hours + "h ";
+        }
+        if (minutes < 1 && hours < 1 && days < 1) {
+            
+        } else {
+            text = text + minutes + "m ";
+        }
+        document.getElementById(id).innerHTML = text + seconds + "s " +"</h4>";
 
         // If the count down is over, write some text 
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("demo").innerHTML = "Auction has ended";
+            document.getElementById(id).innerHTML = "<h4>Auction has ended</h4>";
         }
     }, 1000);
 }
