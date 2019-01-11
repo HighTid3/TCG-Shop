32fc895e251d781ebfd441e362aa88859c585651
Unknown <nol_computernerd@hotmail.com>
2019-01-11 01:47:11 +0100

    Auctionhouse

--- TCGshopTestEnvironment/Controllers/AuctionController.cs
+++ TCGshopTestEnvironment/Controllers/AuctionController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.Encodings.Web;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
@@ -10,12 +11,18 @@ using TCGshopTestEnvironment.Models;
 using TCGshopTestEnvironment.Services;
 using TCGshopTestEnvironment.ViewModels;
 using Microsoft.AspNetCore.Mvc.Routing;
+using Mollie.Api.Client;
+using Mollie.Api.Client.Abstract;
+using Mollie.Api.Models;
+using Mollie.Api.Models.Payment.Request;
+using Mollie.Api.Models.Payment.Response;
 
 namespace TCGshopTestEnvironment.Controllers
 {
     public class AuctionController : Controller
     {
         private DBModel _context;
+        private IPaymentClient paymentClient = new PaymentClient("test_y7mvmmrTRRRJECQxvSwrBSJHdjKuxa");
         private readonly UserManager<UserAccount> _userManager;
         private readonly SignInManager<UserAccount> _signInManager;
         private readonly IEmailSender _emailSender;
@@ -50,59 +57,129 @@ namespace TCGshopTestEnvironment.Controllers
         {
             var user = await _userManager.GetUserAsync(User);
             var HighestBid = _context.AuctionBids.Where(x => x.ProductId == productId).Select(x => x.Bid).DefaultIfEmpty().Max();
-            var AuctionBid = new AuctionBids
-                {Bid = Bid, BidDate = DateTime.Now, ProductId = productId, UserId = user.Id};
-            _context.AuctionBids.Add(AuctionBid);
-            _context.SaveChanges();
-            return Json(new { success = true });
+            if (HighestBid >= Bid)
+            {
+                return Json("test");
+            }
+            else
+            {
+                var secondhighestbidder = _context.AuctionBids.Where(x => x.ProductId == productId)
+                    .OrderByDescending(r => r.Bid).Select(x => x.User).Skip(1).FirstOrDefault();
+                if (secondhighestbidder != null)
+                {
+                    await _emailSender.SendEmailAsync(secondhighestbidder.Email, "You have been Outbidden",
+                        "You have been outbidden on a action you made a bid on");
+                }
+                var AuctionBid = new AuctionBids
+                { Bid = Bid, BidDate = DateTime.Now, ProductId = productId, UserId = user.Id };
+                _context.AuctionBids.Add(AuctionBid);
+                _context.SaveChanges();
+                return Json(new { success = true });
+            }
+
         }
 
         [HttpPost]
         public async Task<IActionResult> RewardEmail()
         {
-            var EndedAuctions = _context.products.Where(x => x.AuctionEndTime < DateTime.Now && x.AuctionEndTime > new DateTime(2017)).ToList();
+            //check which auctions have ended
+            var EndedAuctions = _context.products.Where(x => x.AuctionEndTime < DateTime.Now && x.AuctionEndTime > new DateTime(2017) && !x.Removed).ToList();
 
-                //var HighestBidder = new AuctionDetailViewModel
-                //{
-                //    Id = vm.Id,
-                //    UserName = vm.UserName,
-                //    Email = vm.Email,
-                //    UserId = vm.UserId,
-                //    AuctionEnd = vm.AuctionEnd
-                    
-                //};
-            var highestBid = new List<UserAccount> { };
 
+            var highestBid = new List<HighestBidDetails> { };
+
+            //fill highestbid list with users that have the highest bids on their auctions
             foreach (var Highestbiddings in EndedAuctions)
             {
                 var userbid = _context.AuctionBids.Where(x => x.ProductId == Highestbiddings.ProductId)
-                    .Select(x => x.User).DefaultIfEmpty().FirstOrDefault();
-                if(userbid != null) highestBid.Add(userbid);
-
+                    .DefaultIfEmpty().FirstOrDefault();
+                var userofbid = _context.AuctionBids.Where(x => x.ProductId == Highestbiddings.ProductId).Select(x => x.User)
+                    .DefaultIfEmpty().FirstOrDefault();
+                var highbidID = new HighestBidDetails
+                {
+                    Bid = userbid.Bid,
+                    product = userbid.Product,
+                    User = userofbid
+                };
+                if (userbid != null) highestBid.Add(highbidID);
+                Highestbiddings.Removed = true;
             }
-                   
-        
+
+            _context.SaveChanges();
+
 
 
             foreach (var user in highestBid)
             {
-                var callbackUrl = Url.Action(new UrlActionContext
+                var order = new Order
                 {
-                    Action = "ClaimReward",
-                    Controller = "Auction",
-                    Values = new { userId = user.Id },
-                    Protocol = HttpContext.Request.Scheme
-                });
+                    PaymentStatus = "Waiting for payment",
+                    Address = user.User.Address,
+                    Country = user.User.Country,
+                    Email = user.User.Email,
+                    FirstName = user.User.FirstName,
+                    Guid = Guid.NewGuid(),
+                    LastName = user.User.LastName,
+                    OrderDate = DateTime.Now,
+                    Paid = false,
+                    Phone = user.User.PhoneNumber,
+                    PostalCode = user.User.ZipCode,
+                    Total = user.Bid
 
-                await _emailSender.SendEmailAsync(user.Email, "Auction Won!",
-                    "Please claim your reward by <a href=" + callbackUrl + ">clicking here</a>.");
+                };
+                List<OrderDetail> OrderDetail = new List<OrderDetail>
+                {
+                    new OrderDetail
+                    {
+                        OrderId = order.OrderId,
+                        ProductId = user.product.ProductId,
+                        Quantity = 1,
+                        UnitPrice = user.Bid,
+                        Product = user.product,
+                        Order = order
+                    }
+                };
+                order.OrderDetails = OrderDetail;
+
+                PaymentRequest paymentRequest = new PaymentRequest()
+                {
+                    Amount = new Amount(Currency.EUR, order.Total.ToString("F", CultureInfo.InvariantCulture)),
+                    Description = "Payment for your mock purchase from TCG.Sale",
+                    RedirectUrl = "https://tcg.sale" + Url.Action("Processing", "Checkout") + "?guid=" + order.Guid,
+                    WebhookUrl = "https://tcg.sale" + Url.Action("Webhook", "Checkout")
+                };
+
+                PaymentResponse paymentResponse = await paymentClient.CreatePaymentAsync(paymentRequest);
+                Console.WriteLine(paymentResponse.Links.Checkout);
+
+                //Updating PaymentId
+                order.PaymentId = paymentResponse.Id;
+                order.PaymentStatus = "created";
+
+                _context.Orders.Add(order);
+                await _context.SaveChangesAsync();
+                //var orderurl = "tcg.sale/Manage/OrderDetails?orderid=" + order.OrderId +"";
+                await _emailSender.SendEmailAsync(user.User.Email, "Auction Won!",
+                    "You have won the auction, you can pay by clicking on this link: "+
+                    paymentResponse.Links.Checkout.Href+ " for further details about the card you won, Go to your order page on our website")
+                    ;
             }
-            
+
+
+
 
             return RedirectToAction("AuctionHouse", "Auction");
-                
-            
+
+
             //return View(vm);
         }
     }
+
+    public class HighestBidDetails
+    {
+        public Products product { get; set; }
+        public UserAccount User { get; set; }
+        public decimal Bid { get; set; }
+    }
+
 }
